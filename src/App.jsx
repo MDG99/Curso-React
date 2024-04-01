7c1692c4fb65a614fdd3ce3c8a415d6f1e5e9842
MDG99 <ignacioisaac30@gmail.com>
2024-04-01 14:45:05 -0600

    Fragments

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,29 @@
 // import {App} from './App.jsx'
 
 
+// const App = () => {
+//     return (
+//         // Más de un componente se tiene que devolver en un contenedor
+//         <dv>
+//             <h1 className="text-center"> Hola soy React</h1>
+//             <img src="https://picsum.photos/200/300" alt="" />
+//         </dv>
+//     )
+// }
+
+// Si no queremos el div utilizamos algo llamado fragment, nos permite evitar agregar divs innecesarios
+// No es necesartio importarlo pues se puede hacer con los tags vacíos <></>
+import { Fragment } from "react";
 const App = () => {
     return (
         // Más de un componente se tiene que devolver en un contenedor
-        <dv>
+        <Fragment>
             <h1 className="text-center"> Hola soy React</h1>
             <img src="https://picsum.photos/200/300" alt="" />
-        </dv>
+        </Fragment>
     )
 }
+
 
 // Exportamos
 export default App;
