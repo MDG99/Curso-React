41568a64a10bf3f53d8dc437d8411fb0448fef50
MDG99 <ignacioisaac30@gmail.com>
2024-04-01 14:49:32 -0600

    interpolacion de react

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,17 @@
 
-// Exportaciones nombradas. Al importar lo hacemos con el nombre entre corchetes {App} en el main
-// export const App = () => {
-//     return (<h1>Hola soy React</h1>)
-// }
-// import {App} from './App.jsx'
 
 
-// const App = () => {
-//     return (
-//         // Más de un componente se tiene que devolver en un contenedor
-//         <dv>
-//             <h1 className="text-center"> Hola soy React</h1>
-//             <img src="https://picsum.photos/200/300" alt="" />
-//         </dv>
-//     )
-// }
-
-// Si no queremos el div utilizamos algo llamado fragment, nos permite evitar agregar divs innecesarios
-// No es necesartio importarlo pues se puede hacer con los tags vacíos <></>
-import { Fragment } from "react";
 const App = () => {
+    const title = "mi titulo desde una constante"
+    const classTitle = "text-center"
+    const pathImg = "https://picsum.photos/200/300"
+    
     return (
         // Más de un componente se tiene que devolver en un contenedor
-        <Fragment>
-            <h1 className="text-center"> Hola soy React</h1>
-            <img src="https://picsum.photos/200/300" alt="" />
-        </Fragment>
+        <>
+            <h1 className={classTitle}>{title.toUpperCase()}</h1>
+            <img src={pathImg} alt="" />
+        </>
     )
 }
 
