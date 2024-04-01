c650ecebd9e43ca886644e7d0b19be37e40152ed
MDG99 <ignacioisaac30@gmail.com>
2024-04-01 14:59:51 -0600

    Componentes básicos

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-
+// los componentes siempre van en mayúscula
+const Button = () => {
+    return(
+        <button>I 'am a button</button>
+    )
+}
 
 
 const App = () => {
     const title = "mi titulo desde una constante"
     const classTitle = "text-center"
     const pathImg = "https://picsum.photos/200/300"
-    
+
     return (
         // Más de un componente se tiene que devolver en un contenedor
         <>
             <h1 className={classTitle}>{title.toUpperCase()}</h1>
             <img src={pathImg} alt="" />
+            <Button/>
         </>
     )
 }
